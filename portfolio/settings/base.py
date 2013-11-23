556aa3233401ffe1c0d62dceff2890a2639d1252
Daniel Stephenson <dstephenson@dojodevcamp.com>
2013-11-23 14:52:45 -0700

    Updated static dirs in settings so that public/static was included

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -94,6 +94,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    normpath(join(SITE_ROOT, 'public', 'static')),
     normpath(join(SITE_ROOT, 'static')),
 )
 
