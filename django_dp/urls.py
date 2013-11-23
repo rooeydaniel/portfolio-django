5eec27a438a5bdc0b9242da823219351f36f423c
Daniel Stephenson <dstephenson@dojodevcamp.com>
2013-11-23 12:48:33 -0700

    Base Django Project Template

---
+++ django_dp/urls.py
@@ -0,0 +1,20 @@
+"""
+This is your project's master URL configuration, it defines the set of "root" URLs for the entire project
+"""
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'django_dp.views.home', name='home'),
+    # url(r'^django_dps/', include('django_dp.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
